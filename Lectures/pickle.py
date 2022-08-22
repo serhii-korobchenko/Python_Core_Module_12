984ab0f1b25510072a2e8a25d4502794dcbc3ba6
Serhii Korobchenko <jchild2008@gmail.com>
2022-08-22 09:28:49 +0300

    220822

---
+++ Lectures/pickle.py
@@ -0,0 +1,14 @@
+import pickle
+
+
+some_data = {
+    (1, 3.5): 'tuple',
+    2: [1, 2, 3],
+    'a': {'key': 'value'}
+}
+
+byte_string = pickle.dumps(some_data)
+unpacked = pickle.loads(byte_string)
+
+print(unpacked == some_data)    # True
+print(unpacked is some_data)    # False
